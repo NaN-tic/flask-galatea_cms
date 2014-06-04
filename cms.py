4a448a6934b3b29cde40484928a1139cbb2989d0
resteve <resteve@zikzakmedia.com>
2014-06-04 14:34:36 +0200

    Tryton Transaction

--- cms.py
+++ cms.py
@@ -1,33 +1,30 @@
 from flask import Blueprint, render_template, current_app, abort, g
-from flask_tryton import Tryton
+from galatea.tryton import tryton
 from galatea.utils import get_tryton_locale
 
 cms = Blueprint('cms', __name__, template_folder='templates')
 
+Article = tryton.pool.get('galatea.cms.article')
+
+galatea_website = current_app.config.get('TRYTON_GALATEA_SITE')
+
+@tryton.default_context
+def default_context():
+    language = get_tryton_locale(g.language)
+    return {'language': language}
+
 @cms.route("/<slug>", endpoint="article")
+@tryton.transaction()
 def article(lang, slug):
-    tryton = Tryton(current_app)
-    Article = tryton.pool.get('galatea.cms.article')
+    '''Article detaill'''
+    articles = Article.search([
+        ('slug', '=', slug),
+        ('active', '=', True),
+        ('galatea_website', '=', galatea_website),
+        ], limit=1)
 
-    galatea_website = current_app.config.get('TRYTON_GALATEA_SITE')
-
-    @tryton.default_context
-    def default_context():
-        language = get_tryton_locale(g.language)
-        return {'language': language}
-
-    @tryton.transaction()
-    def _get_article(slug):
-
-        articles = Article.search([
-            ('slug', '=', slug),
-            ('active', '=', True),
-            ('galatea_website', '=', galatea_website),
-            ], limit=1)
-
-        if not articles:
-            abort(404)
-        article, = articles
-        return render_template('cms-article.html',
-                article=article)
-    return _get_article(slug)
+    if not articles:
+        abort(404)
+    article, = articles
+    return render_template('cms-article.html',
+            article=article)
