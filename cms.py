0b510c3e01fa933d29745ca07e9d5362a2d5d19a
resteve <resteve@zikzakmedia.com>
2014-07-03 19:14:45 +0200

    Move default context to main app

--- cms.py
+++ cms.py
@@ -1,17 +1,11 @@
-from flask import Blueprint, render_template, current_app, abort, g
+from flask import Blueprint, render_template, current_app, abort
 from galatea.tryton import tryton
-from galatea.utils import get_tryton_locale
 
 cms = Blueprint('cms', __name__, template_folder='templates')
 
 Article = tryton.pool.get('galatea.cms.article')
 
 galatea_website = current_app.config.get('TRYTON_GALATEA_SITE')
-
-@tryton.default_context
-def default_context():
-    language = get_tryton_locale(g.language)
-    return {'language': language}
 
 @cms.route("/<slug>", endpoint="article")
 @tryton.transaction()
